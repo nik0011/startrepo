335097b423dc18285faf2b4d49d7e61baabb1455
nik001 <nikhiltakle055@gmail.com>
2023-10-15 12:46:50 +0530

    jspratice3

---
+++ 3.js
@@ -0,0 +1,32 @@
+const btn=document.querySelector("#main");
+         const throttleFunction=(func, delay)=>{
+           let prev = 0; 
+      return (...args) => {
+        let now = new Date().getTime();     
+        if(now - prev> delay){ 
+          prev = now;
+          return func(...args);  
+        }
+      }
+    }
+btn.addEventListener("mousemove", throttleFunction((dets)=>{
+       var div = document.createElement("div");
+       div.classList.add("imgdiv");
+       div.style.left= dets.clientX+'px';
+       div.style.top = dets.clientY+'px';
+       var img = document.createElement("img");
+       img.setAttribute("src","https:images.unsplash.com/photo-1696185137293-418bdf31a8d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1412&q=80")
+       div.appendChild(img);
+       document.body.appendChild(div);
+       gsap.to(img,{
+        y:"0",
+        ease:Power1,
+        duration:0.3,
+       });
+       gsap.to(img,{
+        y:"100%",
+        delay:.5,
+        ease:Power1,
+        duration:0.3,
+       });
+}, 50));
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 it consist of small projects
 1. opacity of rectangle will change when the cursor moves from left to right
 2. rectangle will move along with the cursor  
+3. image will pop and go when the cursor move in the box.
